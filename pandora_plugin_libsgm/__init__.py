d4765ce19f79c5d8b2d4555f5713f2a6f2ff69bc
Marie Leconte <marie.leconte@csgroup.eu>
2024-06-05 10:54:53 +0200

    copyright update

--- pandora_plugin_libsgm/__init__.py
+++ pandora_plugin_libsgm/__init__.py
@@ -1,6 +1,6 @@
 #
 # coding: utf8
-# Copyright (c) 2020 Centre National d'Etudes Spatiales (CNES).
+# Copyright (c) 2024 Centre National d'Etudes Spatiales (CNES).
 #
 # This file is part of Pandora plugin LibSGM
 #
